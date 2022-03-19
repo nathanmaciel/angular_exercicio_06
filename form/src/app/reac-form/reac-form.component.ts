a5bb1aaeb097dd7e036ff4b3c7389ba070799ce8
Nathan Maciel <nathanlmsilva@gmail.com>
2022-03-19 23:31:42 +0000

    new

--- form/src/app/reac-form/reac-form.component.ts
+++ form/src/app/reac-form/reac-form.component.ts
@@ -1,10 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormArray, FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ValidateCPFDirective } from '../directives/validate-cpf.directive';
 import { ValidateTelDirective } from '../directives/validate-tel.directive';
+import { ExmpDiagComponent } from '../exmp-diag/exmp-diag.component';
 import { ValidateCPF } from '../rect-validators/cpf.validator';
 import { passValidator } from '../rect-validators/pass.validator';
 import { ValidatePhone } from '../rect-validators/tel.validator';
+import { DialogData, Phones} from '../modules/dialog-data';
 
 
 @Component({
@@ -43,18 +46,21 @@ export class ReacFormComponent{
       compl: new FormControl(''),
       passes: new FormGroup({
         password: new FormControl('',[
+          Validators.required,
           Validators.minLength(6),
           
         ]),
         confPassword: new FormControl('',[
+          Validators.required
         ])
-      },[
-        passValidator,
-      ] )
+      })
   })
 
   phoneArr: FormArray = this.formData.get('phones') as FormArray
 
+  passConference: FormControl = this.formData.get('passes')?.get('confPassword') as FormControl
+
+  
   addPhone(){
     this.phoneArr.push(new FormControl('',[]))
   }
@@ -63,10 +69,34 @@ export class ReacFormComponent{
   }
 
   changeKey(){
-    this.passKey = this.formData.get('password')?.value
+    this.passKey = this.formData.get('passes')?.get('password')?.value
+    this.passConference.addValidators(passValidator(this.passKey))
   }
-  constructor() { 
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(){
+    const dialogRef = this.dialog.open(ExmpDiagComponent, {
+      width: '250px',
+      data: {
+        name: this.formData.get('name')?.value,
+        lastName: this.formData.get('lastName')?.value, 
+        username: this.formData.get('username')?.value, 
+        cpf: this.formData.get('cpf')?.value, 
+        adress: this.formData.get('adress')?.value, 
+        compl: this.formData.get('compl')?.value, 
+        password: this.formData.get('passes')?.get('password')?.value
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      // if(result == '') {
+      //   // console.log(`%cUsuário ${this.userNum}`, 'font-size: 20px;')
+      //   // console.log('Dados Corrigidos')
+      // }
+      // if (result == true);
+    });
   }
 
 }
-
--- form/src/app/rect-validators/pass.validator.ts
+++ form/src/app/rect-validators/pass.validator.ts
@@ -1,14 +1,24 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
-export function passValidator(control: AbstractControl) {
-  console.log('aaa')
-  console.log(control.value.password)
+// export function passValidator(control: AbstractControl) {
+//   console.log('aaa')
+//   console.log(control.value.password)
 
-  if (control.value.password != control.value.confPassword && control.value.confPassword.length > 0){
-    console.log('iguais')
-    return {'passValidation': true}
-  }
+//   if (control.value.password != control.value.confPassword && control.value.confPassword.length > 0){
+//     console.log('iguais')
+//     return {'passValidation': true}
+//   }
 
-  console.log('null')
-  return null;
+//   console.log('null')
+//   return null;
+// }
+
+export function passValidator(pass: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    console.log(`control: ${control.value}  |  pass ${pass}`)
+    const forbidden = (control.value != pass);
+    console.log(`forb1: ${forbidden}`)
+    return forbidden ? {validationPass: {value: control.value}} : null;
+  };
 }
+
